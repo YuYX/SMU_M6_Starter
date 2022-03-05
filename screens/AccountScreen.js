5aec4d53a4aaf9fcdbce2db13a70880f89467b20
YuYX <yuyongxue@gmail.com>
2022-03-05 16:53:28 +0800

    5th commit on 5 Mar.

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,31 +1,36 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Image,
-  Switch,
-  Text,
-  TouchableOpacity,
-  View,
+import React, { useState, useEffect } from "react";
+import { 
+  ActivityIndicator, 
+  TouchableOpacity, 
+  Text, 
+  View, 
+  Switch, 
+  Image, 
+  Animated, 
+  TouchableWithoutFeedback,
 } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
+import { commonStyles, lightStyles, darkStyles } from "../styles/commonStyles";
+import axios from "axios";
 import { API, API_WHOAMI } from "../constants/API";
-import { changeModeAction } from "../redux/ducks/accountPref";
+import { useDispatch, useSelector } from "react-redux";
+import { changeModeAction } from '../redux/ducks/accountPref';
 import { logOutAction } from "../redux/ducks/blogAuth";
-import { commonStyles, darkStyles, lightStyles } from "../styles/commonStyles";
-
 export default function AccountScreen({ navigation }) {
   const [username, setUsername] = useState(null);
-
+  
   const token = useSelector((state) => state.auth.token);
-
   const isDark = useSelector((state) => state.accountPref.isDark);
-  const profilePicture = useSelector(
-    (state) => state.accountPref.profilePicture
-  );
+  const profilePicture = useSelector((state) => state.accountPref.profilePicture);
   const dispatch = useDispatch();
 
-  const styles = { ...commonStyles, ...(isDark ? darkStyles : lightStyles) };
+  const picSize = new Animated.Value(200);
+  const sizeInterpolation = {
+    inputRange: [0, 0.5, 1],
+    outputRange: [200, 300, 200]
+  }  
+
+  const styles = { ...commonStyles, ...isDark ? darkStyles : lightStyles };
+
   async function getUsername() {
     console.log("---- Getting user name ----");
     console.log(`Token is ${token}`);
@@ -41,7 +46,7 @@ export default function AccountScreen({ navigation }) {
         console.log(error.response.data);
         if (error.response.data.status_code === 401) {
           signOut();
-          navigation.navigate("SignInSignUp");
+          navigation.navigate("SignInSignUp")
         }
       } else {
         console.log(error);
@@ -49,14 +54,22 @@ export default function AccountScreen({ navigation }) {
       // We should probably go back to the login screen???
     }
   }
-
   function signOut() {
-    dispatch(logOutAction());
+    dispatch(logOutAction())
     navigation.navigate("SignInSignUp");
   }
-
   function switchMode() {
-    dispatch(changeModeAction());
+    dispatch(changeModeAction())
+  }
+
+  function changePicSize() {
+    Animated.loop(
+      Animated.timing(picSize, {
+        toValue: 300,
+        duration: 2500,
+        useNativeDriver: false
+      }),
+    ).start()
   }
 
   useEffect(() => {
@@ -70,47 +83,30 @@ export default function AccountScreen({ navigation }) {
     getUsername();
     return removeListener;
   }, []);
-
   return (
     <View style={[styles.container, { alignItems: "center" }]}>
-      <Text style={[styles.title, styles.text, { marginTop: 30 }]}>
-
-        {" "}
-
-        Hello {username} !
-
-      </Text>
-
-      <Image
-
-        source={{ uri: profilePicture }}
-
-        style={{ width: 250, height: 250, borderRadius: 200 }}
-
-      />
-
+      <Text style={[styles.title, styles.text, { margin: 30 }]}> Hello {username} !</Text>
+      {profilePicture == null ? <View /> :
+        <TouchableWithoutFeedback onPress={changePicSize}>
+          <Animated.Image 
+            style={{ width: picSize, height: picSize, borderRadius: 200 }} 
+            source={{ uri: profilePicture }} />
+        </TouchableWithoutFeedback>
+      }
       <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-
-        <Text style={{ marginTop: 10, fontSize: 20, color: "#0000EE" }}>
-
-          {" "}
-          No profile picture. Click to take one.{" "}
-        </Text>
-      </TouchableOpacity>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          margin: 20,
-        }}
-      >
+          <Text style={{ marginTop: 10, fontSize: 20, color: "#0000EE" }}> No profile picture. Click to take one. </Text>
+          </TouchableOpacity>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", margin: 20}}>
         <Text style={[styles.content, styles.text]}> Dark Mode? </Text>
-        <Switch value={isDark} onChange={switchMode} />
+        <Switch
+          value={isDark}
+          onChange={switchMode}/>
       </View>
       <TouchableOpacity style={[styles.button]} onPress={signOut}>
-        <Text style={styles.buttonText}>Sign Out</Text>
-      </TouchableOpacity>
+        <Text style={styles.buttonText}>
+          Sign Out
+        </Text>
+        </TouchableOpacity>
     </View>
   );
 }
